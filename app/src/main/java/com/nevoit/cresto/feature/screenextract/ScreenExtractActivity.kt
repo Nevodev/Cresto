7fd52eb31f15d4bcd57f807c629e1f7e3441639f
Nevoit <sunanbang2007@qq.com>
2026-05-17 17:51:05 +0800

    fix: use new api instead causes many bugs. rollback the changes to use deprecated api.

--- app/src/main/java/com/nevoit/cresto/feature/screenextract/ScreenExtractActivity.kt
+++ app/src/main/java/com/nevoit/cresto/feature/screenextract/ScreenExtractActivity.kt
@@ -2,7 +2,6 @@ package com.nevoit.cresto.feature.screenextract
 
 import android.app.Activity
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 
 @Suppress("DEPRECATION")
@@ -11,11 +10,6 @@ class ScreenExtractActivity : Activity() {
         super.onCreate(savedInstanceState)
         startService(Intent(this, ScreenExtractService::class.java))
         finish()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            overrideActivityTransition(OVERRIDE_TRANSITION_OPEN, 0, 0)
-        } else {
-            @Suppress("DEPRECATION")
-            overridePendingTransition(0, 0)
-        }
+        overridePendingTransition(0, 0)
     }
 }
